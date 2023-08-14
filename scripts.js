3c04e0a5659877fd6a78d77721dcbc821aa28380
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2023-08-14 17:14:58 +0100

    Added buttons

--- scripts.js
+++ scripts.js
@@ -2,3 +2,28 @@ const addition = (a, b) => a + b;
 const subtraction = (a, b) => a - b;
 const multiplication = (a, b) => a * b;
 const division = (a, b) => a / b;
+
+let firstNumber = 0;
+let operator;
+let secondNumber = 0;
+
+function operate(firstNumber, secondNumber, operator) {
+  let result = 0;
+
+  switch (operator) {
+    case "add":
+      result = addition(firstNumber, secondNumber);
+      break;
+    case "subtraction":
+      result = subtraction(firstNumber, secondNumber);
+      break;
+    case "multiplication":
+      result = multiplication(firstNumber, secondNumber);
+      break;
+    case "division":
+      result = division(firstNumber, secondNumber);
+      break;
+    default:
+      break;
+  }
+}
